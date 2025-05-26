67a2949e56eb3fdcb5a771152c28c335e2c3b259
jmj <waysoleward01@naver.com>
2025-05-26 15:37:48 +0900

    feat: 리포지토리 설정

---
+++ src/main/java/com/packup/admin/admin/domain/repository/AdminInfoRepository.java
@@ -0,0 +1,7 @@
+package com.packup.admin.admin.domain.repository;
+
+import com.packup.admin.admin.domain.AdminInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminInfoRepository extends JpaRepository<AdminInfo, Long> {
+}
