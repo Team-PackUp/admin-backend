082bad23b519c589ea2b0653dda0de0788a56bff
jmj <waysoleward01@naver.com>
2025-05-26 13:58:41 +0900

    init

---
+++ src/main/java/com/packup/admin/PackupAdminApplication.java
@@ -0,0 +1,13 @@
+package com.packup.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackupAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackupAdminApplication.class, args);
+    }
+
+}
