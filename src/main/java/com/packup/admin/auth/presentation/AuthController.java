89260751cd07a7eac094268460cc5da2b8684c3b
jmj <waysoleward01@naver.com>
2025-05-26 15:34:33 +0900

    feat: 컨트롤러 세팅

---
+++ src/main/java/com/packup/admin/auth/presentation/AuthController.java
@@ -0,0 +1,15 @@
+package com.packup.admin.auth.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+//    @PostMapping("/login")
+//    public
+}
