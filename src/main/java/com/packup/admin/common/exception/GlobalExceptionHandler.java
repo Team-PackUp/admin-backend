44b3a5631a6526e7eb2f5d8b300716daa8a11bce
jmj <waysoleward01@naver.com>
2025-06-25 00:21:44 +0900

    feat: 전역 예외 처리

--- src/main/java/com/packup/admin/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/packup/admin/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.packup.admin.common.exception;
 
 import com.packup.admin.auth.exception.AuthException;
+import com.packup.admin.auth.exception.AuthExceptionType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,11 +12,16 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(AuthException.class)
-//    public ResponseEntity<?> handleAuthException(AuthException ex) {
-//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-//                .body(Map.of("message", ex.getMessage()));
-//    }
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<?> handleAuthException(AuthException ex) {
+        if (ex.exceptionType() == AuthExceptionType.UNAUTHORIZED_REQUEST) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", ex.getMessage()));
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("message", ex.getMessage()));
+    }
 
 
 }
