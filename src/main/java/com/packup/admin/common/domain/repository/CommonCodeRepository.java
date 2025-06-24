189e12b6f813fed90ec1e291790d9fe7022026d2
jmj <waysoleward01@naver.com>
2025-06-24 16:32:13 +0900

    feat: 공통 코드 추가

---
+++ src/main/java/com/packup/admin/common/domain/repository/CommonCodeRepository.java
@@ -0,0 +1,11 @@
+package com.packup.admin.common.domain.repository;
+
+import com.packup.admin.common.domain.CommonCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CommonCodeRepository extends JpaRepository<CommonCode, String> {
+    // 중복이면 2개 나올 듯
+    Optional<CommonCode> findByCodeName(String codeName);
+}
