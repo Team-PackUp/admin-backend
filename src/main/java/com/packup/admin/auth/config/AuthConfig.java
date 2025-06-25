663899e092877aae38867f4c32a37b2f3a47bb1a
jmj <waysoleward01@naver.com>
2025-06-25 16:03:00 +0900

    feat: @Auth 애너테이션 사용을 위한 argresolver 설정

---
+++ src/main/java/com/packup/admin/auth/config/AuthConfig.java
@@ -0,0 +1,26 @@
+package com.packup.admin.auth.config;
+
+import com.packup.admin.auth.presentation.AuthArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthConfig implements WebMvcConfigurer {
+    private final AuthArgumentResolver authArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
+    }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/api/files/**")
+//                .addResourceLocations("file:///D:/projects/packup/files/");
+//    }
+}
