5c42daa08e654db6b4209aeb0fe7073f753be1a1
jmj <waysoleward01@naver.com>
2025-06-29 18:37:06 +0900

    feat: PATCH 메서드 추가 - CORS

--- src/main/java/com/packup/admin/config/security/CorsConfig.java
+++ src/main/java/com/packup/admin/config/security/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(allowedOrigin));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
 
